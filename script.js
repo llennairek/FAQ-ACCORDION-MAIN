3425a15008a98dc3969a37f424507cda6940900b
llennairek <llennairek@gmail.com>
2021-02-14 15:41:19 +0100

    seems that the click event triggered space or enter key so simplify the JS

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const questionsContainer = document.querySelectorAll(".questions-container");
 const questions = document.querySelectorAll(".questions");
 
 function display(event) {
-  console.log(event.target);
+  console.log(event);
   const childrens = this.children;
   if(!childrens[2].classList.contains("active")) {
     childrens[0].classList.add("questions-active");
@@ -16,24 +16,4 @@ function display(event) {
 
 questionsContainer.forEach(container => {
   container.addEventListener("click", display)
-})
-
-function displayAccess(event){
-  const childrens = (event.target.parentNode).children;
-  if(!childrens[2].classList.contains("active")) {
-    childrens[0].classList.add("questions-active");
-    childrens[2].classList.add("active");
-    childrens[1].classList.add("arrow-active"); 
-  } else {
-    childrens[0].classList.remove("questions-active");
-    childrens[2].classList.remove("active");  
-    childrens[1].classList.remove("arrow-active"); 
-}
-};
-
-questions.forEach(question => {
-  question.addEventListener("keydown", event => {
-    if(event.key === " " || event.key === "Enter"){
-      displayAccess(event)
-  }})
 })
